1269eac53793c55c2561c01e35fee3f33877dac8
Damian Ferro <dfmartinez8909@gmail.com>
2021-06-24 18:05:26 +0200

    Java built-in dynamic arrays handler ArrayList

--- src/com/home/dev/Main.java
+++ src/com/home/dev/Main.java
@@ -1,10 +1,12 @@
 package com.home.dev;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
+       //Manual dynamic array
         Array numbers = new Array(3);
         numbers.insert(10);
         numbers.insert(20);
@@ -14,6 +16,18 @@ public class Main {
         numbers.removeAt(4);
         numbers.print();
         System.out.println(numbers.indexOf(1));
+
+        //Java built-in dynamic arrays handler
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(50);
+        list.add(60);
+        list.add(70);
+        list.add(80);
+        list.remove(1);
+        list.indexOf(70);
+        list.contains(100);
+        System.out.println(list);
+
 
     }
 }
